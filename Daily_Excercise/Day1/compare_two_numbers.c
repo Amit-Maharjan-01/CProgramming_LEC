7e098139299bfefc2f888f432dd1e9db10c575e5
Amit Maharjan <amitmaharjan2002@gmail.com>
2022-02-17 17:42:17 +0545

    Comparing Two Numbers

--- Daily_Excercise/Day1/compare_two_numbers.c
+++ Daily_Excercise/Day1/compare_two_numbers.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-void main()
+void main ()
 {
+    int a,b,big;
+    printf("Enter two numbers : ");
+    scanf("%d %d",  &a, &b);
 
+    (a > b) ? (big = a) : (big = b);
+    printf("The biggest among a and b is %d\n", big);
 }
