e8ea3a137054115d62a13bd3d901d6d4224491af
Amit Maharjan <amitmaharjan2002@gmail.com>
2022-03-02 20:25:07 +0545

    pattern

--- pattern/pattern2.c
+++ pattern/pattern2.c
@@ -36,8 +36,67 @@ void rect()
   }
 }
 
+//wap to print any given number into V shape with one side ascending and one side descending order.
+
+void Vshape()
+{
+  int n, i, j, k, p = 0;
+  printf("Enter the top number : ");
+  scanf("%d", &n);
+
+  for(i = n; i >= 1; i--)
+  {
+    for(k = 1; k < 1; k++)
+    {
+      printf(" ");
+    }
+    for(j = i; j <= n+p; j++)
+    {
+      if(j==i||j==n+p)
+      printf("%d",j);
+
+      else
+      {
+        printf(" ");
+      }
+      
+    }
+    p++;
+    printf("\n");
+  }
+}
+
+// WAP to print 1 2 3 4 5
+//                2 3 4 5
+//                  3 4 5
+//                    4 5
+//                      5
+
+void inv_tri()
+{
+   int n, k;
+  printf("Enter the number you want to make triangle of : ");
+  scanf("%d", &n);
+
+  for(int i = 1; i <= n; i++)
+  {
+    for(int k = 0; k <= i-1; k++)
+    {
+      printf("\t");
+    }
+    for(int j = i; j <= n; j++)
+    {
+      printf("%d\t",j);
+    }
+    printf("\n");
+  }
+}
+
+
 void main()
 {
     //pattern();
-    rect();
+   // rect();
+   //Vshape();
+   inv_tri();
 }
