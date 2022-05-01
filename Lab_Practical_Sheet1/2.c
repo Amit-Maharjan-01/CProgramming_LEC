4019843de536d32d3e7eb4717f90fabf524b3714
Amit Maharjan <amitmaharjan2002@gmail.com>
2022-05-01 21:48:04 +0545

    sum,product and quotient

---
+++ Lab_Practical_Sheet1/2.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+
+void main ()
+{
+    int sum, product, quotient;
+
+    sum = 22 + 7;
+    product = 22 * 7;
+    quotient = 22/7;
+
+    printf("The sum is: %d\n The product is: %d\n The quotient is: %d", sum, product, quotient);
+}
