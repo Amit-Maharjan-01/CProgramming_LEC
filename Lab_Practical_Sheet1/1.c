eae26f65b431233f25d9f16e5ec460d450257456
Amit Maharjan <amitmaharjan2002@gmail.com>
2022-05-01 21:47:06 +0545

    calculating volume

---
+++ Lab_Practical_Sheet1/1.c
@@ -0,0 +1,14 @@
+//wap to calculate the volume of pool.The equation for determining the volume is
+// Volume = length * width * depth. Assume that the pool has a length of 25 feet,
+//a width of 10 feet,and depth of 6 feet.
+
+ #include<stdio.h>
+
+ void main()
+ {
+    int v, l = 25, w = 10, d = 6;
+
+    v = l*w*d;
+
+    printf("The volume of given pool is %d", v);
+ }
